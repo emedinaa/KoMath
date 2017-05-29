f154c6d13af93caeca988e5166e1337cde26b034
Eduardo Medina <emedinaa@gmail.com>
2017-05-29 16:30:55 -0500

    update unit test

---
+++ SampleKotlin/app/src/test/kotlin/komath/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by eduardomedina on 29/05/17.
+ * https://github.com/JetBrains/kotlin-examples/tree/master/gradle/android-junit-test
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect(){
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
